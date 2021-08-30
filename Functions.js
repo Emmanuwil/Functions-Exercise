08c5c7cb7bcc06d84fa83a3704ca96c5b54b8c7f
Emmanuwil <simonemmanuwil@gmail.com>
2021-08-29 22:49:27 -0400

    First Commit

---
+++ Functions.js
@@ -0,0 +1,14 @@
+//Exercise 1
+
+ function printOdds(count) {
+     for (let i = 1; 1 < count; i++) {
+         if (i % 2 == 1) {
+             console.log(i);
+         } else { odd
+             continue
+         }
+     }
+ }
+
+// Exercise 2
+ 
